eef5af80eac79775c36621c4a856f1a4d7177385
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2023-02-22 17:51:21 -0600

    initial commit, basic file structure created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <header className='app-header'>
+        <h1 className='title'>Axe App</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
